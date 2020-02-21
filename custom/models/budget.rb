670bbf6454a55dada0eaf816cffdc5f93e92e08a
Andriy Iun <andriy.yun@gmail.com>
2020-02-21 14:13:26 +0100

    Update consul reference. Changed default value for fields

--- custom/models/budget.rb
+++ custom/models/budget.rb
@@ -3,5 +3,5 @@
 require_dependency Rails.root.join('app', 'models', 'budget').to_s
 
 class Budget < ApplicationRecord
-  CURRENCY_SYMBOLS = %w[€ $ £ ¥ DKK].freeze
+  CURRENCY_SYMBOLS = %w[DKK € $ £ ¥].freeze
 end
