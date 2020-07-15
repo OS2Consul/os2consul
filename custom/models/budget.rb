65068d687c0cb153614b8d87605ff456e11d4113
Andriy Iun <andriy.yun@gmail.com>
2020-07-15 16:04:56 +0200

    Removed extra currency

--- custom/models/budget.rb
+++ custom/models/budget.rb
@@ -3,5 +3,5 @@
 require_dependency Rails.root.join('app', 'models', 'budget').to_s
 
 class Budget < ApplicationRecord
-  CUSTOM_CURRENCY_SYMBOLS = %w[USD DKK € $ £ ¥].freeze
+  CUSTOM_CURRENCY_SYMBOLS = %w[DKK € $ £ ¥].freeze
 end
