b00f0776006cfae7b3d64aec9358b989ebfbcafd
Logan Raarup <logan@logan.dk>
2022-01-04 12:43:39 +0000

    Only configure SAML when metadata URL is specified

--- custom/config/initializers/omniauth_saml.rb
+++ custom/config/initializers/omniauth_saml.rb
@@ -1,13 +1,15 @@
-idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
-idp_metadata = idp_metadata_parser.parse_remote_to_hash(Rails.application.secrets.saml_metadata_url)
-idp_metadata[:issuer] = Rails.application.secrets.saml_issuer
-
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :saml, idp_metadata
-end
-
-Devise.setup do |config|
-  config.omniauth :saml,
-    idp_cert_fingerprint: idp_metadata[:idp_cert_fingerprint],
-    idp_sso_target_url: idp_metadata[:idp_sso_target_url]
+if Rails.application.secrets.saml_metadata_url.present?
+  idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
+  idp_metadata = idp_metadata_parser.parse_remote_to_hash(Rails.application.secrets.saml_metadata_url)
+  idp_metadata[:issuer] = Rails.application.secrets.saml_issuer
+  
+  Rails.application.config.middleware.use OmniAuth::Builder do
+    provider :saml, idp_metadata
+  end
+  
+  Devise.setup do |config|
+    config.omniauth :saml,
+      idp_cert_fingerprint: idp_metadata[:idp_cert_fingerprint],
+      idp_sso_target_url: idp_metadata[:idp_sso_target_url]
+  end
 end
