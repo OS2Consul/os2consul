6f9445c4c8cd09f92eb392913d003f1c3a834783
Logan Raarup <logan@logan.dk>
2022-01-04 20:57:25 +0100

    Prevent infinite redirect on cookie consent

--- custom/controllers/cookies_controller.rb
+++ custom/controllers/cookies_controller.rb
@@ -2,6 +2,11 @@ class CookiesController < ApplicationController
   skip_authorization_check
 
   def consent
+    # Prevent infinite redirects
+    if cookies[:consent_given]
+      redirect_to root_path
+      return
+    end
     cookies[:consent_given] = Time.current.to_s
     redirect_back(fallback_location: root_path)
   end
