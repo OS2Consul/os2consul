dac614e0d12789832909b2ed0387953902bddb2f
Logan Raarup <logan@logan.dk>
2022-02-07 13:17:15 +0100

    Fix omniauth-saml issues after upgrade

--- custom/config/initializers/omniauth_saml.rb
+++ custom/config/initializers/omniauth_saml.rb
@@ -1,15 +1,17 @@
+OmniAuth.config.allowed_request_methods = [:get, :post]
+
 if Rails.application.secrets.saml_metadata_url.present?
   idp_metadata_parser = OneLogin::RubySaml::IdpMetadataParser.new
   idp_metadata = idp_metadata_parser.parse_remote_to_hash(Rails.application.secrets.saml_metadata_url)
   idp_metadata[:issuer] = Rails.application.secrets.saml_issuer
-  
+
   Rails.application.config.middleware.use OmniAuth::Builder do
     provider :saml, idp_metadata
   end
-  
+
   Devise.setup do |config|
     config.omniauth :saml,
       idp_cert_fingerprint: idp_metadata[:idp_cert_fingerprint],
-      idp_sso_target_url: idp_metadata[:idp_sso_target_url]
+      idp_sso_target_url: idp_metadata[:idp_sso_service_url]
   end
 end
