61092b67085f77447236ce33fce63a5af5718e35
andriyun <andriy.yun@gmail.com>
2022-05-04 15:50:05 +0200

    Adjusted Budget landing lage to listing

---
+++ custom/controllers/budgets_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require_dependency Rails.root.join('app', 'controllers', 'budgets_controller').to_s
+
+class BudgetsController < ApplicationController
+  def index
+    @finished_budgets = @budgets.finished.order(created_at: :desc)
+    @current_filter ||= "open"
+    @budgets = Budget.send(@current_filter).order(created_at: :desc).page(params[:page])
+  end
+end
