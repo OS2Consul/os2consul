dff176b3424ce73980c96e0bbcf837a5a81214a6
andriyun <andriy.yun@gmail.com>
2022-06-22 13:19:59 +0200

    Added ability to change email on account view page

--- custom/controllers/account_controller.rb
+++ custom/controllers/account_controller.rb
@@ -3,6 +3,12 @@
 require_dependency Rails.root.join('app', 'controllers', 'account_controller').to_s
 
 class AccountController < ApplicationController
+  alias original_update update
+  def update
+    @account.skip_reconfirmation!
+    return original_update
+  end
+
   private
 
   def account_params
@@ -11,7 +17,7 @@ class AccountController < ApplicationController
                    [:phone_number, :email_on_comment, :email_on_comment_reply, :newsletter,
                     organization_attributes: %i[name responsible_name]]
                  else
-                   %i[public_activity public_interests email_on_comment
+                   %i[email public_activity public_interests email_on_comment
                       email_on_comment_reply email_on_direct_message email_digest newsletter
                       official_position_badge recommended_debates recommended_proposals]
                  end
