17f876e19f98e10ed14c44ee3abf913a8598140e
Logan Raarup <logan@logan.dk>
2022-12-02 11:07:13 +0000

    Add restore button to draft proposals

--- custom/controllers/admin/hidden_proposals_controller.rb
+++ custom/controllers/admin/hidden_proposals_controller.rb
@@ -8,4 +8,12 @@ class Admin::HiddenProposalsController < Admin::BaseController
     original_index
     @draft_proposals = Proposal.draft.sort_by_created_at
   end
+
+  alias original_restore restore
+  def restore
+    if @proposal.draft?
+      @proposal.publish
+    end
+    original_restore
+  end
 end
