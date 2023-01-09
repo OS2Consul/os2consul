e22b86937c30db5a18aeacc72942c8c8880c45ae
Logan Raarup <logan@logan.dk>
2023-01-09 16:25:15 +0100

    Add SAML NameID related settings

--- custom/controllers/users/omniauth_callbacks_controller.rb
+++ custom/controllers/users/omniauth_callbacks_controller.rb
@@ -21,16 +21,20 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def saml
-    auth = request.env["omniauth.auth"]
+    auth = request.env['omniauth.auth']
 
     # Fetching user data from attributes.
     attributes = auth.extra.response_object.attributes
-    username = attributes["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier"].first()
-    fullname = attributes["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"].first()
-    email = attributes["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress"].first()
+    username = if [true, 1, 'true', '1'].include? Rails.application.secrets.saml_use_email_as_nameid
+                 attributes['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'].first
+               else
+                 attributes['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier'].first
+               end
+    fullname = attributes['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name'].first
+    email = attributes['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/emailaddress'].first
 
     # Requires OneLogin::RubySaml::Attributes.single_value_compatibility = false in config/initializers/omniauth-saml.rb
-    groups = auth.extra.response_object.attributes["http://schemas.xmlsoap.org/claims/Group"]
+    groups = auth.extra.response_object.attributes['http://schemas.xmlsoap.org/claims/Group']
 
     user = User.find_by(email: email) || User.new(
       username: username,
@@ -45,37 +49,37 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       consent_and_information: '1'
     )
 
-    if user.save
-      # Checking up moderator rights. Add it if missing.
-      if groups && Rails.application.secrets.sso_moderator_group
-        moderatorRegex = '/^' + Rails.application.secrets.sso_moderator_group + '/'
-        Rails.logger.debug("Found setting for SSO group " + Rails.application.secrets.sso_moderator_group + ". Checking if group present in SSO attributes")
-        if groups.grep(moderatorRegex)
-          Rails.logger.debug("Group has been found in SSO attributes. Check moderators right")
-          if !Moderator.where(user_id: user.id).exists?
-            Rails.logger.debug("User is not moderator. Adding modetators rights.")
-            user.moderator = Moderator.new
-            user.save!
-          end
+    return unless user.save
+
+    # Checking up moderator rights. Add it if missing.
+    if groups && Rails.application.secrets.sso_moderator_group
+      moderatorRegex = '/^' + Rails.application.secrets.sso_moderator_group + '/'
+      Rails.logger.debug('Found setting for SSO group ' + Rails.application.secrets.sso_moderator_group + '. Checking if group present in SSO attributes')
+      if groups.grep(moderatorRegex)
+        Rails.logger.debug('Group has been found in SSO attributes. Check moderators right')
+        unless Moderator.where(user_id: user.id).exists?
+          Rails.logger.debug('User is not moderator. Adding modetators rights.')
+          user.moderator = Moderator.new
+          user.save!
         end
       end
-
-      # Checking up administrator rights. Add it if missing.
-      if groups && Rails.application.secrets.sso_administrator_group
-        administratorRegex = '/^' + Rails.application.secrets.sso_administrator_group + '/'
-        Rails.logger.debug("Found setting for SSO group " + Rails.application.secrets.sso_administrator_group + ". Checking if group present in SSO attributes")
-        if groups.grep(administratorRegex)
-          Rails.logger.debug("Group has been found in SSO attributes. Check administrator right")
-          if !Administrator.where(user_id: user.id).exists?
-            Rails.logger.debug("User is not administrator. Adding administrator rights.")
-            user.administrator = Administrator.new
-            user.save!
-          end
-        end
-      end
-
-      flash[:notice] = t('devise.sessions.signed_in')
-      sign_in_and_redirect user, event: :authentication
     end
+
+    # Checking up administrator rights. Add it if missing.
+    if groups && Rails.application.secrets.sso_administrator_group
+      administratorRegex = '/^' + Rails.application.secrets.sso_administrator_group + '/'
+      Rails.logger.debug('Found setting for SSO group ' + Rails.application.secrets.sso_administrator_group + '. Checking if group present in SSO attributes')
+      if groups.grep(administratorRegex)
+        Rails.logger.debug('Group has been found in SSO attributes. Check administrator right')
+        unless Administrator.where(user_id: user.id).exists?
+          Rails.logger.debug('User is not administrator. Adding administrator rights.')
+          user.administrator = Administrator.new
+          user.save!
+        end
+      end
+    end
+
+    flash[:notice] = t('devise.sessions.signed_in')
+    sign_in_and_redirect user, event: :authentication
   end
 end
