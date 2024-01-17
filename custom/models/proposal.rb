736ab86be32f63c1d686997158340302bf12195b
Logan Raarup <logan@logan.dk>
2024-01-17 15:06:27 +0000

    Archive proposals based on modation date, publishing date or creation date, in that order

--- custom/models/proposal.rb
+++ custom/models/proposal.rb
@@ -4,4 +4,14 @@ class Proposal < ApplicationRecord
   def editable_by?(user)
     (author_id == user.id || user.administrator?) && editable?
   end
+
+  def archived?
+    if ignored_flag_at.present?
+      ignored_flag_at <= Setting["months_to_archive_proposals"].to_i.months.ago
+    elsif published_at.present?
+      published_at <= Setting["months_to_archive_proposals"].to_i.months.ago
+    else
+      created_at <= Setting["months_to_archive_proposals"].to_i.months.ago
+    end
+  end
 end
